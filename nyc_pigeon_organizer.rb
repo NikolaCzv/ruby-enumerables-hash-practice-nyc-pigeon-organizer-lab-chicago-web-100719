6a3738bc9df9463fce0bb50a57f1c9639341f030
Nikola Raicic <nikola.raicic@hotmail.com>
2019-10-02 20:16:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
 new_hash = {}
 data.each do |key, value|
-  value.each do |new value, names|
+  value.each do |new_value, names|
     names.each do |name|
       
       if !new_hash[name]
